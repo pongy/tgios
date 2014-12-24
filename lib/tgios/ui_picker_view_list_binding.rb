aa157dcffaf322ea161ab221508cc3cba6c3eade
April Tsang <april@tofugear.com>
2014-12-24 13:39:48 +0800

    picker binding add text color option

--- lib/tgios/ui_picker_view_list_binding.rb
+++ lib/tgios/ui_picker_view_list_binding.rb
@@ -8,10 +8,11 @@ module Tgios
       @events[event_name]=block.weak!
     end
 
-    def bind(picker_view, list: list, display_field: display_field)
+    def bind(picker_view, options={})
+      @options = options
       @picker_view=WeakRef.new(picker_view)
-      @list=WeakRef.new(list)
-      @display_field=display_field
+      @list=WeakRef.new(options[:list])
+      @display_field=options[:display_field]
       @picker_view.dataSource=self
       @picker_view.delegate=self
     end
@@ -26,12 +27,22 @@ module Tgios
     end
 
     def pickerView(pickerView, titleForRow: row, forComponent: component)
-      record = @list[row]
-      record.is_a?(Hash) ? record[@display_field] : record.send(@display_field)
+      get_display_text(row)
+    end
+
+    def pickerView(pickerView, attributedTitleForRow:row, forComponent:component)
+      if @options[:text_color]
+        NSAttributedString.alloc.initWithString(get_display_text(row), attributes:{NSForegroundColorAttributeName => @options[:text_color]})
+      end
     end
 
     def pickerView(pickerView, didSelectRow:row, inComponent:component)
       @events[:row_selected].call(row, selected_record) unless @events[:row_selected].nil?
+    end
+
+    def get_display_text(row)
+      record = @list[row]
+      record.is_a?(Hash) ? record[@display_field] : record.send(@display_field)
     end
 
     def selected_record
